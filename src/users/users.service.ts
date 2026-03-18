762e2eec7e8ccb0622a8188cd75f0910ec5ad285
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-03-18 20:36:28 +0200

    add users service

---
+++ src/users/users.service.ts
@@ -0,0 +1,60 @@
+import { Injectable, ConflictException, NotFoundException, Inject } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User, UserRole } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(userData: Partial<User>) {
+    const existingUser = await this.usersRepository.findOne({ where: { email: userData.email } });
+    if (existingUser) {
+      throw new ConflictException('User with this email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    const user = this.usersRepository.create({
+      ...userData,
+      password: hashedPassword,
+    });
+
+    return this.usersRepository.save(user);
+  }
+
+  async findByEmail(email: string) {
+    return this.usersRepository.findOne({ 
+      where: { email },
+      select: ['id', 'email', 'password', 'role', 'fullName', 'isActive']
+    });
+  }
+
+  async findOne(id: string) {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  async findAll() {
+    return this.usersRepository.find();
+  }
+
+  async update(id: string, updateData: Partial<User>) {
+    if (updateData.password) {
+      updateData.password = await bcrypt.hash(updateData.password, 10);
+    }
+    await this.usersRepository.update(id, updateData);
+    return this.findOne(id);
+  }
+
+  async remove(id: string) {
+    const user = await this.findOne(id);
+    return this.usersRepository.remove(user);
+  }
+}
