eac0b76f6984cf9d8eed5fe509f0501133d14594
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-03-18 20:36:07 +0200

    add user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,37 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  CUSTOMER = 'customer',
+}
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column()
+  fullName: string;
+
+  @Column({
+    type: 'simple-enum',
+    enum: UserRole,
+    default: UserRole.CUSTOMER,
+  })
+  role: UserRole;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
